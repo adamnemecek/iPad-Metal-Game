123d2d8f2c6b4a36ed0562fff34b60f2bcaaa1a9
andrew clear <andrewclear@hotmail.com>
2016-05-24 13:38:30 +0900

    Added the beginnings of the base object class.

---
+++ iPadGame/BaseObject.swift
@@ -0,0 +1,24 @@
+//
+//  BaseObject.swift
+//  iPadGame
+//
+//  Created by Andrew Clear on 5/24/16.
+//  Copyright © 2016 Andrew Clear. All rights reserved.
+//
+
+import Foundation
+import simd
+
+class BaseObject:NSObject
+{
+    var worldPosition:float3;
+    
+    override init()
+    {
+        worldPosition = float3.init(x:0.0, y:0.0, z:0.0)
+        
+        super.init()
+    }
+    
+}
+
